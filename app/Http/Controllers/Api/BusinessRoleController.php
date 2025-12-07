138078bfddce1cc75fd17708df0565a7784948de
Nityanand5 <nityanandjha13578@gmail.com>
2025-12-07 12:10:20 +0530

    `Removed unused imports`

--- app/Http/Controllers/Api/BusinessRoleController.php
+++ app/Http/Controllers/Api/BusinessRoleController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\BusinessRole;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BusinessRoleResource;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
